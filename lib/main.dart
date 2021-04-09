064e667c66d7fa4538e0f66b92e69de7b6fb40f3
Zereef <rafael18.vv@gmail.com>
2021-04-09 18:31:14 +0100

    Main menu

--- lib/main.dart
+++ lib/main.dart
@@ -1,58 +1,92 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
-
-import 'package:test_app/runtime_variables.dart';
-
+import 'package:hive/hive.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import 'Theme/config.dart';
 import 'marcarConsulta.dart';
 
-void main() {
+import 'package:test_app/runtime_variables.dart';
+import 'package:test_app/Theme/ThemeData.dart';
+import 'package:test_app/Theme/CurrentTheme.dart';
+
+Future<void> main() async {
+  await Hive.initFlutter();
+  box = await Hive.openBox('easyTheme');
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+CurrentTheme currentTheme = CurrentTheme();
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  void initState() {
+    super.initState();
+    currentTheme.addListener(() {
+      print('changes');
+      setState(() {});
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    SystemChrome.setEnabledSystemUIOverlays([]);
     return MaterialApp(
       //Debug features
       showPerformanceOverlay: false,
       debugShowCheckedModeBanner: false,
-      //
+      theme: AppTheme.lightTheme,
       darkTheme: ThemeData.dark(),
-      theme: ThemeData(primarySwatch: Colors.pink),
+      themeMode: currentTheme.currentTheme(),
       title: 'Flutter Demo',
-      home: MyHomePage(title: 'General Widget Develop'),
       routes: {
         '/marcarConsulta': (context) => MarcarConsulta(),
       },
+      home: Main(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-  final String title;
+class Main extends StatefulWidget {
+  const Main({
+    Key key,
+  }) : super(key: key);
 
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  _MainState createState() => _MainState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
+class _MainState extends State<Main> {
   @override
   Widget build(BuildContext context) {
     Common.width = MediaQuery.of(context).size.width;
     Common.height = MediaQuery.of(context).size.height;
     return Scaffold(
-      appBar: AppBar(title: Text(widget.title)),
-      body: Center(),
+      appBar: AppBar(
+        title: Text("General Widget Develop"),
+        actions: [
+          TextButton(
+            child: Text(currentTheme.currentTheme() == ThemeMode.dark ? "Dark" : "Light"),
+            onPressed: () => currentTheme.switchTheme(),
+          ),
+        ],
+      ),
+      body: Center(
+        child: TextButton(
+          child: Text("Foo"),
+          onPressed: () => Navigator.pushNamed(context, "/marcarConsulta"),
+        ),
+      ),
 
       //old stuff
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          Navigator.pushNamed(context, "/marcarConsulta");
-        },
-        child: Icon(Icons.add),
-      ),
+      // floatingActionButton: FloatingActionButton(
+      //   onPressed: () {
+      //     currentTheme.switchTheme();
+      //   },
+      //   child: Icon(Icons.add),
+      // ),
     );
   }
 }
