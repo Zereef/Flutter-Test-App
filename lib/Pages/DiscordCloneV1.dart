fef31e17a54f4198d028edbcdf9349833e6971f7
Zereef <rafael18.vv@gmail.com>
2021-07-15 17:20:42 +0100

    discord recriation start

---
+++ lib/Pages/DiscordCloneV1.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class DiscordCloneV1 extends StatefulWidget {
+  const DiscordCloneV1({Key key}) : super(key: key);
+
+  @override
+  _DiscordCloneV1State createState() => _DiscordCloneV1State();
+}
+
+class _DiscordCloneV1State extends State<DiscordCloneV1> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.red,
+    );
+  }
+}
